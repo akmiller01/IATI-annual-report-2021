c2f0ff9be4a5f4062abd547489adad9050a60bf4
akmiller01 <alex.k.miller@gmail.com>
2021-03-17 18:01:26 -0400

    codelist

--- print_indicators.R
+++ print_indicators.R
@@ -93,9 +93,13 @@ length(setdiff(unique_iati_identifiers_2019,unique_iati_identifiers))
 
 # Activity count with SDG Tags
 a_count_sdg = stack(a_count_by_sdg)
+sdg_goals = fread("../UNSDG-Goals.csv")[,c("code","name")]
+setnames(sdg_goals,"code","sdg")
+setnames(sdg_goals,"name","goal")
 setnames(a_count_sdg,"ind","sdg")
 setnames(a_count_sdg,"values","activities")
-a_count_sdg = a_count_sdg[,c("sdg","activities")]
+a_count_sdg = merge(a_count_sdg,sdg_goals,by="sdg")
+a_count_sdg = a_count_sdg[,c("sdg","goal","activities")]
 a_count_sdg = a_count_sdg[order(-a_count_sdg$activities),]
 fwrite(a_count_sdg,"activities_count_by_sdg.csv")
 
